ecdc069c7e533406118c531998a8cbc7944e58eb
Serdar Onur Karadağ <98060048+soKaradag@users.noreply.github.com>
2023-05-26 23:36:21 +0300

    Created Custom CollectionViewCell

--- ProgrammaticUI/ProgrammaticUI/Managers/APICaller.swift
+++ ProgrammaticUI/ProgrammaticUI/Managers/APICaller.swift
@@ -20,7 +20,7 @@ enum APIError: Error {
 class APICaller {
     static let shared = APICaller()
     
-    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+    func getTrendingMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day?api_key=\(Constants.API_KEY)") else {return}
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {
@@ -28,17 +28,17 @@ class APICaller {
             }
             
             do {
-                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 completion(.success(results.results))
             } catch  {
-                completion(.failure(error))
+                completion(.failure(APIError.failedTogetData))
             }
             
         }
         task.resume()
     }
     
-    func getTrendingTvs(completion: @escaping (Result<[Tv], Error>) -> Void) {
+    func getTrendingTvs(completion: @escaping (Result<[Title], Error>) -> Void) {
         guard let url = URL(string: "\(Constants.baseURL)/3/trending/tv/day?api_key=\(Constants.API_KEY)") else {return}
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {
@@ -46,16 +46,16 @@ class APICaller {
             }
             
             do {
-                let result = try JSONDecoder().decode(TrendingTvResponse.self, from: data)
+                let result = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 print(result)
             } catch {
-                print(error.localizedDescription)
+                completion(.failure(APIError.failedTogetData))
             }
         }
         task.resume()
     }
     
-    func getUpcomingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+    func getUpcomingMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
         guard let url = URL(string: "\(Constants.baseURL)/3/movie/upcoming?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {
@@ -63,17 +63,17 @@ class APICaller {
             }
             
             do {
-                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 completion(.success(results.results))
             } catch  {
-                completion(.failure(error))
+                completion(.failure(APIError.failedTogetData))
             }
 
         }
         task.resume()
     }
     
-    func getPopularMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+    func getPopularMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
         guard let url = URL(string: "\(Constants.baseURL)/3/movie/popular?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {
@@ -81,17 +81,17 @@ class APICaller {
             }
             
             do {
-                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 completion(.success(results.results))
             } catch  {
-                completion(.failure(error))
+                completion(.failure(APIError.failedTogetData))
             }
 
         }
         task.resume()
     }
     
-    func getTopRated(completion: @escaping (Result<[Movie], Error>) -> Void) {
+    func getTopRated(completion: @escaping (Result<[Title], Error>) -> Void) {
         guard let url = URL(string: "\(Constants.baseURL)/3/movie/top_rated?api_key=\(Constants.API_KEY)&language=en-US&page=1") else {return}
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {
@@ -99,10 +99,10 @@ class APICaller {
             }
             
             do {
-                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                let results = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
                 completion(.success(results.results))
             } catch  {
-                completion(.failure(error))
+                completion(.failure(APIError.failedTogetData))
             }
 
         }
---
+++ ProgrammaticUI/ProgrammaticUI/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  TitleCollectionViewCell.swift
+//  ProgrammaticUI
+//
+//  Created by Serdar Onur KARADAĞ on 26.05.2023.
+//
+
+import UIKit
+import SDWebImage
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    public func configure(with model: String) {
+        guard let url = URL(string: model) else {return}
+        posterImageView.sd_setImage(with: url, completed: nil)
+    }
+}
