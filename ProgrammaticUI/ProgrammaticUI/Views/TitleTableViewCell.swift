970af81f617b30b21ef8df96655d40a36e9cc28d
Serdar Onur Karadağ <98060048+soKaradag@users.noreply.github.com>
2023-05-27 18:32:16 +0300

    Created Custom TableViewCell for UpcomingTable

---
+++ ProgrammaticUI/ProgrammaticUI/Views/TitleTableViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  TitleTableViewCell.swift
+//  ProgrammaticUI
+//
+//  Created by Serdar Onur KARADAĞ on 27.05.2023.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+    static let identifier = "TitleTableViewCell"
+    
+    private let playTitleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let titlePosterUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(titlePosterUIImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(playTitleButton)
+        
+        applyConstraints()
+    }
+    
+    private func applyConstraints() {
+        let titlePostImageVİewConstraints = [
+            titlePosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            titlePosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            titlePosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        NSLayoutConstraint.activate(titlePostImageVİewConstraints)
+    }
+    
+    public func configure(with model: TitleViewModel) {
+        guard  let url = URL(string: model.posterURL) else {return}
+        titlePosterUIImageView.sd_setImage(with: url, completed: nil)
+        titleLabel.text = model.titleName
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
